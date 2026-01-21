f6e52bb4d5aeb776c7c120291afd8d748819134b
ZAYA CODE HUB <zayacodehub@gmail.com>
2026-01-21 12:22:53 +0000

    REVERT THIS CHANGES

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body
         className={cn(
           'min-h-screen bg-background font-body antialiased',
