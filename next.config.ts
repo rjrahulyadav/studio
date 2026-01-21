2f83ab1a814c59337bb0afa293752405ac54a4b2
ZAYA CODE HUB <zayacodehub@gmail.com>
2026-01-21 13:40:31 +0000

    add this photo in director

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.dhanihyasolutions.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
